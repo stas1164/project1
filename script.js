b64b5e6679388a02f8ae9c3457be4242e2bb87e2
Stas <example@gmail.com>
2019-11-09 23:31:39 +1100

    First

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+
+let persone = {
+name: "John",
+age: 25,
+isMarried: false 
+};
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[0]);
+
+//alert('Hello');
+//let answer = confirm('Are you here');
+//console.log(answer);
+let answer = prompt('Есть 18','Да');
+console.log(typeof(answer));
